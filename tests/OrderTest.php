280fbf1714f7357284114d76db973fcda064aa64
Roberto Gianotto <gianottoroberto@gmail.com>
2019-05-01 12:45:58 +0200

    Update OrderTest.php with Mockery Framework
    Added a new tests for Order class using Mockery framework instead classic phpunit mock tecnique

--- tests/OrderTest.php
+++ tests/OrderTest.php
@@ -6,6 +6,11 @@ require 'src/Order.php';
 
 class OrderTest extends TestCase
 {
+    public function tearDown()
+    {
+        Mockery::close();
+    }
+
     public function testOrderIsProcessedWithAClassThatNotExists()
     {
         // così becco un warning devo usare il mockBuilder
@@ -19,6 +24,22 @@ class OrderTest extends TestCase
             ->method('charge')
             ->with($this->equalTo(200))
             ->willReturn(true);
+
+        $order = new Order($gateway);
+
+        $order->amount = 200;
+
+        $this->assertTrue($order->process());
+    }
+
+    public function testOrderIsProcessWithMockeryFramework()
+    {
+        $gateway = Mockery::mock('PaymentGateway');
+
+        $gateway->shouldReceive('charge')
+            ->once()
+            ->with(200)
+            ->andReturn(true);
 
         $order = new Order($gateway);
 
